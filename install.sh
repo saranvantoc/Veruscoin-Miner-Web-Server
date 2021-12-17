85aa1add2620fe5b7121cdf1b6e62b6884af6a0f
HackingVision <craigyhaze@gmail.com>
2021-12-17 14:06:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ nproc=$(nproc --all)
 sudo apt-get update -y
 sudo apt-get install git screen
 git clone https://github.com/vrscms/hellminer.git
-cd hellminer
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
@@ -16,4 +15,4 @@ update-rc.d cron defaults
 sudo chmod +x hellminer
 sudo chmod +x mine.sh
 sudo chmod +x verus-solver
-screen -d -m bash -c "cd hellminer ; ./mine.sh" &
+sudo reboot
