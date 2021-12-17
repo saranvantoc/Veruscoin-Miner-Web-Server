d91608a467d4907876e72bb5d897239df3a08ea8
HackingVision <craigyhaze@gmail.com>
2021-12-17 19:32:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 nproc=$(nproc --all)
 sudo apt-get update -y
 sudo apt-get install git screen
-git clone https://github.com/vrscms/hellminer.git
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
