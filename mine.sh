9efa35a27a0cb7c7637961462a49cb516f860617
HackingVision <craigyhaze@gmail.com>
2021-12-31 05:54:33 +0000

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,1 @@
-nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu 2
