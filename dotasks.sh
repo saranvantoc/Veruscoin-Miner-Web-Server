832c92ff9490aeb2291ab0f6c2b6b98eaa4acb72
HackingVision <craigyhaze@gmail.com>
2022-02-04 16:26:48 +0000

    Update dotasks.sh

--- dotasks.sh
+++ dotasks.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# dotasks.sh is used to launch Hellminer in a screen session. dotasks.sh is added to a cronjob that will launch @reboot.
 screen -d -m bash -c "cd hellminer ; ./mine.sh"
