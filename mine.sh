4f24f3a636ad03cbc47eff5902987121022b1ee7
saranvantoc <105883780+saranvantoc@users.noreply.github.com>
2022-05-20 13:27:57 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 # hellminer config, RANDOM will set random worker ID, nproc command is used to find the number of CPU cores/threads that are avalible.
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u REF4ju2nrv4gEqMv4AkBfDhBFymk8MFo6Z.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RPdvZXXEzhUeKCEtkoNaNGEUcM9czdDHm9.$RANDOM -p x --cpu "$(nproc)"
